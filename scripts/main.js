e933d48c5d0bd597d9d43d8f5a0410aebb18d51c
Daniel <holadanielam@gmail.com>
2023-10-08 20:52:42 -0500

    Primera actualizacion

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "La curvatura de Gauss";
+
+document.querySelector("html").addEventListener("click", () => {
+  alert("Ouch! Stop poking me!");
+});
