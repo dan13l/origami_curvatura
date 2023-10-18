1e4bda7cee5d2fc458c6ac57798353bd3ee97c8e
Daniel <holadanielam@gmail.com>
2023-10-17 21:54:58 -0500

    JS basico

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,23 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "La curvatura de Gauss";
 
-document.querySelector("html").addEventListener("click", () => {
+
+document.querySelector("img").addEventListener("click", () => {
+  //Ejemplo de un listener
   alert("Ouch! Stop poking me!");
 });
+
+
+function createParagraph() {
+  // Agrega el parrafo <p>
+  const para = document.createElement("p");
+  para.textContent = "You clicked the button!";
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
+
