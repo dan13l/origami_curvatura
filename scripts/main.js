dc7c53110a1f828ca80b866a960e943d69b16092
Dan13l <holadanielam@gmail.com>
2023-10-20 15:02:10 -0500

    funciones de prueba 12345

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,51 @@
-
 document.querySelector("img").addEventListener("click", () => {
-  //Ejemplo de un listener
+  //Ejemplo de un listener que actua solo sobre la imagen
   alert("Ouch! Stop poking me!");
 });
 
 
+//Creo un Div nuevo para ingresar un texto despues de activar un boton.
 const body = document.body;
-//const body = document.getElementById("body")
 
-
-//Como selecciono el lugar para colocar el texto y no que siempre este de ultimo
+//¿Como selecciono el lugar del DOM para colocar lo que se inserta? <main> no sirve...
 const panel = document.createElement("div");
 panel.setAttribute("class", "picaSale");
 body.appendChild(panel);
 
-
-//Como hago para que aparezca despues del Nav, que es fijo del html.
 const msg = document.createElement("p");
-msg.textContent = "This is a message box";
+msg.textContent = "Esta caja desaparecerá, jaja";
 panel.appendChild(msg);
 
+const picaloBt = document.createElement("button");
+picaloBt.textContent = "aja x";
+panel.appendChild(picaloBt);
+
+picaloBt.addEventListener("click", () =>
+  panel.parentNode.removeChild(panel));
+
+
+const msjBt = document.createElement("button");
+msjBt.setAttribute("class", "otro");
+msjBt.textContent = "Click me, el otro";
+panel.appendChild(msjBt);
+
+msjBt.addEventListener("click", createParagraph);
 
 function createParagraph() {
   // Agrega el parrafo <p>
   const para = document.createElement("p");
   para.textContent = "You clicked the button!";
-  document.body.appendChild(para);
+  body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll("button");
+const colorBt = body.querySelector("button");
 
-
-for (const button of buttons) {
-  button.addEventListener("click", createParagraph);
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
 }
 
-
-
-
-const picaElBoton = body.createElement("button");
-picaElBoton.textContent = "aja x";
-panel.appendChild(picaElBoton);
-
-
-
-picaElBoton.addEventListener("click", () =>
-  panel.parentNode.removeChild(panel),
-);
-
-
+colorBt.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  //picaloBt.style.backgroundColor = rndCol;
+  body.style.backgroundColor = rndCol;
+});
