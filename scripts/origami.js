c166a0ef3652d7c2fd0b0ea327a83bd894b7997c
Daniel <holadanielam@gmail.com>
2023-11-19 21:31:06 -0500

    patron lineal

--- scripts/origami.js
+++ scripts/origami.js
@@ -1,51 +1,127 @@
 //Toma el elemento de la pagina para insertar el origami.
 const caja = document.getElementById("papel");
 
-// Creo el elemento de rabbit ear
+// Creo el elemento svg de rabbit ear sobre el cual se va a dibujar
 var dibujo = ear.svg()
-dibujo.size(2, 1);
 
-const base = {
-  vertices_coords: [
-      //Ordenados a partir de (0,0) y en el sentido de las manecillas del relog, El centro es 7
-      [0,0], [0.5,0], [1, 0], [0.75, Math.sqrt(3)/4], [0.5, Math.sqrt(3)/2], [0.25, Math.sqrt(3)/4], [0.5,1/(2*Math.sqrt(3))],
-    ],
-  edges_vertices: [
-      [0, 1], [1, 2], [2, 3],[3,4],[4,5],[5,0], //Triangulo exterior 0,1,2,3,4,5
-      [0,6],[6,3],[2,6],[6,5],[4,6],[6,1], // Mediatrices 6,7, 8,9, 10,11
-  ],
-  faces_vertices: [[0,1,6],[1,2,6],[2,3,6],[3,4,6],[4,5,6],[5,0,6]],
-  edges_assignment: [
-      "M", "M", "M","M", "M","M", //triangulo exterior
-      "V","M", "V","M", "V","M",
-  ]
+// define el tamaño del area en que se dibuja el svg. esquina superior izq (x,y), esquina inf derecha (x,y)
+dibujo.size(-0.1, -0.1, 8.1, 5.1);
+dibujo.scale(1, -1)
+//dibujo.background('black');
+
+
+//Esta es la estructura base. Para utilizar Push es preciso declarar antes los arreglos
+var base = ear.graph();
+base.vertices_coords = [];
+base.edges_vertices = [];
+base.edges_assignment = [];
+
+const fil = 2;
+const col = 10;
+const cent = 1/( 2 * Math.sqrt(3) );
+const EPSILON = 0.001 ;
+
+const dx = 0.25;
+const dy = Math.sqrt(3)/4;
+
+var nv = 0; //numero de vertices existentes
+
+
+// Crea la estructura de vertices y edges con asignaciones. Se crea un triangulo a la vez y luego se junta.
+for (let i = 0; i < col; i++) {
+    for (let j = 0; j < fil; j++) {
+        // las posiciones de cada columna cambian dependiendo si es par o impar
+        if (i%2 === 0) {
+            var posx= i - (i*dx);
+            var posy= 2*j*dy;
+        } else {
+            var posx= i - (i*dx);
+            var posy= (2*j+1)*dy;
+        }
+
+        //Establesco el numero actual de vertices antes de agregar los nuevos
+        nv = ear.graph.count(base, "vertices");
+
+        // Crea los vertices
+        base.vertices_coords.push( // inicia en el centro baja al centro y gira contrario al reloj por fuera
+            [ posx + 2*dx , posy + cent ], //centro
+            [ posx + 2*dx , posy ], [ posx + 4*dx , posy ], // base del triangulo sospecha
+            [ posx + 3*dx , posy + dy ], // costado
+            [ posx + 2*dx , posy + 2*dy ], // punta del triangulo
+            [ posx + dx , posy + dy ], // costado
+            [ posx , posy ] //ultimo
+        );
+
+        // Crea las aristas
+        base.edges_vertices.push(
+            [nv, nv+1],
+            [nv+1, nv+2], [nv+2, nv+3], [nv+3,nv+4], [nv+4,nv+5], [nv+5,nv+6], [nv+6,nv+1], // Triangulo exterior 1,2,3,4,5,6
+            [nv,nv+3], [nv,nv+5], // 0*,7,8 Mediatrices Montaña
+            [nv,nv+2], [nv,nv+4], [nv,nv+6] // 9,10,11 Mediatrices Valle
+        );
+
+        // Asigna los pliegues, las caras se llenan despues de terminada la red con graph.populate(), ver despues del loop
+        base.edges_assignment.push(
+            "M", "M", "M", "M", "M", "M", "M", "M", "M",
+            "V", "V", "V" );
+    }
 }
 
-//base.vertices_coords= base.vertices_coords*0.5
+// Elimina duplicados
+ear.graph.clean(base, EPSILON)
 
-/* Hay que organizar un loop que nos de el graph completo
-const graph = {
-  vertices_coords: [[0, 1], [0.7, 1], [0.7, 0.5]],
-  edges_vertices: [[0, 1], [1, 2], [2, 0]],
-  faces_vertices: [[0,1,2]],
-  edges_assignment: ["M", "M", "V"]
-}*/
+// Establece los limites externos.
 
-//Establece el cambio de color
-const hsl = (hue) => { return "hsl(" + hue + ", 100%, 50%)"; }
+//ear.graph.transform(graph, matrix)
 
+//cuenta la cantidad de vertices
+nv = ear.graph.count(base, "vertices");
+
+//Establece las caras del pliegue una vez han sido definidos las aristas.
+base.populate();
+
+
+//dibuja los vertices
 dibujo.origami.vertices(base)
     .childNodes
     .forEach((circle, i, arr) => circle
         .radius(0.03)
-        .fill(hsl(i + 180)));
+        .fill("white"));
 
-dibujo.origami.edges(base).strokeWidth(0.01);
+dibujo.origami.edges(base).strokeWidth(0.01); // dibuja los edges con el patron de pliegues
 
-/* Algo asi deberia intentar
-dibujo.size(ear.svg.origami.getViewBox(base))
-	.padding(SIZE / 20)
-	.strokeWidth(SIZE / 80);
-*/
+//mostrar los numeros de los vertices
+/*Toca arreglar el texto*/
+dibujo.scale(1, -1)
+    .text( nv.toString() , 2, 4.8)
+    .fill('gold')
+    .fontSize('3px');
+
 
 caja.appendChild(dibujo)
+
+/*
+// Estructura de un triangulo.
+var nuevo = {
+    vertices_coords: [ // inicia en el centro baja al centro y gira contrario al reloj por fuera
+        [ posx + 2*dx, posy + cent ], //centro
+        [ posx + 2*dx, posy ], [ posx + 4*dx, posy ], // base del triangulo sospecha
+        [ posx + 3*dx, posy + dy ], // costados
+        [ posx + 2*dx, posy + 2*dy ], // punta del triangulo
+        [ posx + dx, posy + dy ],
+        [ posx, posy ], //ultimo
+    ],
+    edges_vertices: [
+        [0, 1],
+        [1, 2], [2, 3], [3,4], [4,5], [5,6], [6,1], // Triangulo exterior 1,2,3,4,5,6
+        [0,3], [0,5], // 0*,7,8 Mediatrices Montaña
+        [0,2], [0,4], [0,6], // 9,10,11Mediatrices Valle
+    ],
+    faces_vertices: [ [0,1,9], [9,2,7], [7,3,10], [8,4,10], [8,5,11], [11,6,0] ],
+    edges_assignment: [
+        "M", "M", "M", "M","M","M", "M", "M", "M",
+        "V", "V", "V",
+    ],
+};
+base=nuevo;
+*/
