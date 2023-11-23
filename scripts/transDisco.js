6922bdeac3d061bc0fe5a0df529938cc4c2b9e17
Dan13l <holadanielam@gmail.com>
2023-11-23 12:23:19 -0500

    Listos los patrones -plano y disco- con bordes.

--- scripts/transDisco.js
+++ scripts/transDisco.js
@@ -15,8 +15,8 @@ var patron = ear.graph();
 patron.vertices_coords = [];
 patron.edges_vertices = [];
 patron.edges_assignment = [];
-const fil = 3; //6
-const col = 36; //48 -- Realmente son col + 1/3*col
+const fil = 6; //6
+const col = 48; //48 -- Realmente son col + 1/3*col
 const da = Math.PI/(2*col);
 const r = 2.5 ;
 
@@ -53,7 +53,7 @@ const EPSILON = 0.001 ;
 Debo aumentar en un tercio la cantidad de columnas debido a posda= 3*i*da
 elimino uno para que el algoritmo que detecta los bordes funcione al interior
 */
-for (let i = 0; i < col+1/3*col -1; i++) {
+for (let i = 0; i < col+1/3*col; i++) {
     for (let j = 0; j < fil; j++) {
         posda= 3*i*da; //no se porque funciona, pero asi es...
         // Los radios y distancias dependen de la fila en que estamos
@@ -89,68 +89,47 @@ for (let i = 0; i < col+1/3*col -1; i++) {
             [nv,nv+2], [nv,nv+4], [nv,nv+6] // 9,10,11 Mediatrices Valle
         );
 
+
         // Asigna los pliegues, las caras se llenan despues de terminada la red con graph.populate(), ver despues del loop
-        patron.edges_assignment.push(
-            "M", "M", "M", "M", "M", "M", "M", "M", "M",
-            "V", "V", "V"
+        if (j === 0) {
+        // Agrega el borde de la base
+            if (i%2 === 0) {
+                patron.edges_assignment.push(
+                    "M", "B", "B", "M", "M", "B", "B", "M", "M",
+                    "V", "V", "V"
+                );
+            }  else {
+                patron.edges_assignment.push(
+                    "M", "B", "M", "M", "M", "M", "B", "M", "M",
+                    "V", "V", "V"
+                );
+            }
+        } else if ( j === fil-1) {
+            if (i%2 === 0) {
+                patron.edges_assignment.push(
+                    "M", "M", "M", "B", "B", "M", "M", "M", "M",
+                    "V", "V", "V"
+                );
+            } else {
+                patron.edges_assignment.push(
+                    "M", "M", "B", "B", "B", "B", "M", "M", "M",
+                    "V", "V", "V"
+                );
+            }
+        } else {
+            // Asigna los pliegues, las caras se llenan despues de terminada la red con graph.populate(), ver despues del loop
+            patron.edges_assignment.push(
+                "M", "M", "M", "M", "M", "M", "M", "M", "M",
+                "V", "V", "V"
             );
+        }
+
     }
 }
-
 
 ear.graph.clean(patron, EPSILON);
 
 patron.populate();
-
-// Establece los limites externos.
-var sospechosos = ear.graph.getPlanarBoundary(patron);
-
-let i = 0;
-for (ed in sospechosos["edges"]) {
-    patron.edges_assignment[ sospechosos["edges"][i] ] = "B";
-    i++;
-}
-
-//Ahora debo completar el disco y asignar M-V a quienes deben, se trata de solo la ultima columna
-
-
-for (let j = 0; j < fil; j++) {
-    posda= -3*da; //no se porque funciona, pero asi es...
-    // Los radios y distancias dependen de la fila en que estamos
-    r1 = radios[2*j+1];
-    r2 = radios[2*j+2];
-    r3 = radios[2*j+3];
-
-    //Establesco el numero actual de vertices antes de agregar los nuevos
-    nv = ear.graph.count(patron, "vertices");
-
-    //iniciemos con circulo externo [1] [2] [6]
-    patron.vertices_coords.push(
-        [ w/2 + (r3+2*(r1-r3)/3)*Math.cos(posda + 2*da) , h/2 + (r3+ 2*(r1-r3)/3)*Math.sin(posda + 2*da) ], //  mediana (centro) [0] el radio esta aproximado [ posx + 2*dx , posy + cent ]
-        [ w/2 + r1*Math.cos(posda + 2*da) , h/2 + r1*Math.sin(posda + 2*da) ],  // base 1 del triangulo [1] [ posx + 2*dx , posy ],
-        [ w/2 + r1*Math.cos(posda + 4*da) , h/2 + r1*Math.sin(posda + 4*da) ],  // base 2 del triangulo [2] [ posx + 2*dx , posy ]
-        [ w/2 + r2*Math.cos(posda + 3*da) , h/2 + r2*Math.sin(posda + 3*da) ],  // mediatriz 1 [3] [ posx + 3*dx , posy + dy ],
-        [ w/2 + r3*Math.cos(posda + 2*da) , h/2 + r3*Math.sin(posda + 2*da) ],  // punta del triangulo [4] [ posx + 2*dx , posy + 2*dy ],
-        [ w/2 + r2*Math.cos(posda + da) , h/2 + r2*Math.sin(posda + da) ],      // mediatriz 2 [5] [ posx + dx , posy + dy ],
-        [ w/2 + r1*Math.cos(posda) , h/2 + r1*Math.sin(posda) ]                 //ultimo (base 3 del triangulo) [6] [ posx , posy ]
-    );
-
-    // Crea las aristas
-    patron.edges_vertices.push(
-        [nv, nv+1],
-        [nv+1, nv+2], [nv+2, nv+3], [nv+3,nv+4], [nv+4,nv+5], [nv+5,nv+6], [nv+6,nv+1], // Triangulo exterior 1,2,3,4,5,6
-        [nv,nv+3], [nv,nv+5], // 0*,7,8 Mediatrices Montaña
-        [nv,nv+2], [nv,nv+4], [nv,nv+6] // 9,10,11 Mediatrices Valle
-    );
-
-    // Asigna los pliegues, las caras se llenan despues de terminada la red con graph.populate(), ver despues del loop
-    patron.edges_assignment.push(
-        "M", "M", "M", "M", "M", "M", "M", "M", "M",
-        "V", "V", "V"
-        );
-}
-
-
 
 //cuenta la cantidad de vertices
 nv = ear.graph.count(patron, "vertices");
